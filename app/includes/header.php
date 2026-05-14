99c20598e18c3eb91b1e2f0561fe6dd5f7afccab
Josue Obed <josueobed13@gmail@gmail.com>
2026-05-14 00:04:02 -0500

    fix gulp production

--- app/includes/header.php
+++ app/includes/header.php
@@ -47,20 +47,8 @@ if (!defined('BASE_URL')) {
     <!-- =========================================
          CSS
     ========================================== -->
-
-    <link
-        rel="preload"
-        href="<?= BASE_URL; ?>build/css/app.css"
-        as="style"
-        onload="this.onload=null;this.rel='stylesheet'"
-    >
-
-    <noscript>
-        <link
-            rel="stylesheet"
-            href="<?= BASE_URL; ?>build/css/app.css"
-        >
-    </noscript>
+<link rel="preload" href="<?= BASE_URL; ?>build/css/app.css" as="style">
+<link rel="stylesheet" href="<?= BASE_URL; ?>build/css/app.css">
 
     <!-- =========================================
          FONTS
